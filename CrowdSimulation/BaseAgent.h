d2113ff70cce38f84186a4bb6819395de9a02af0
voidwalker <xuechao.chen@outlook.com>
2019-01-21 16:34:55 +0800

    update

--- CrowdSimulation/BaseAgent.h
+++ CrowdSimulation/BaseAgent.h
@@ -8,15 +8,12 @@ public:
 	IAgent();
 	virtual ~IAgent();
 
+	void test()
+	{
+		auto goal = m_pRVOAgent->getGoalPos();
+		m_pRVOAgent->setGoalPos(goal);
+	}
 
 private:
-	int m_Index;
-	float m_Speed;
-	bool m_RealGoal;
-	int m_EscapeTime;
-	int m_RoadmapIndex;
-	glm::vec2  m_InitPosition;
-	std::vector<glm::vec2> m_Roadmap;
-
 	RVO::Agent* m_pRVOAgent = nullptr;
 };
